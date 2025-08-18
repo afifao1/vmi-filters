d5b2de1473ea24786be2674de5cb54e73197169a
afifao1 <afifao1>
2025-08-18 15:12:46 +0500

    PATCH: Refactor App component to always render FooterAbout and remove conditional rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";          
-import FooterAbout from "./components/Footer/FooterAbout.jsx"; 
+
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 
-export default function App() {
-  const { pathname } = useLocation();
-  const isAbout = pathname === "/about";
+import FooterAbout from "./components/Footer/FooterAbout.jsx";
 
+export default function App() {
   return (
     <>
       <Header />
@@ -17,7 +15,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Home />} />
       </Routes>
-      {isAbout ? <FooterAbout /> : <Footer />}
+      <FooterAbout />
     </>
   );
 }
